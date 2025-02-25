540c99baf2ba30d5c9d7f36c8fef0cc136dfd7c1
chz <2844095003>
2025-02-25 21:02:34 +0800

    课表查看做了一点，毫无头绪

--- src/views/schedule/ShowSchedule.vue
+++ src/views/schedule/ShowSchedule.vue
@@ -1,14 +1,79 @@
 <template>
   <h1>课表查看</h1>
+  <hr>
+  <div class="Table">
+    <el-table
+    :data="tableData"
+    height="600"
+    style="width: 100%"
+    :row-class-name="tableRowClassName"
+  >
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+  </div>
 </template>
 
-<script>
-export default {
-    name:'ShowSchedule'
-    
+<script lang="ts" setup name="ShowSchedule">
+interface User {
+  date: string
+  name: string
+  address: string
 }
+
+const tableRowClassName = ({
+  row,
+  rowIndex,
+}: {
+  row: User
+  rowIndex: number
+}) => {
+  if (rowIndex === 1) {
+    return 'warning-row'
+  } else if (rowIndex === 3) {
+    return 'success-row'
+  }
+  return ''
+}
+
+const tableData: User[] = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
 </script>
 
 <style>
-
+.el-table .warning-row {
+  --el-table-tr-bg-color: var(--el-color-warning-light-9);
+}
+.el-table .success-row {
+  --el-table-tr-bg-color: var(--el-color-success-light-9);
+}
+.el-table{
+  border: 3px solid black;
+}
+.el-table th, td {
+      border: 1px solid gray; /* 灰色边框 */
+      padding: 8px;
+      text-align: left;
+    }
 </style>
