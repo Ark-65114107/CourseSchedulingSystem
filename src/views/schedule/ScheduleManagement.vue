f13593ebf8019298ae86376ea8abbdb54291b582
SmallCarrotQWQ <395680955@qq.com>
2025-03-01 17:22:29 +0800

    改变下一步按钮的位置

--- src/views/schedule/ScheduleManagement.vue
+++ src/views/schedule/ScheduleManagement.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="index">
     <div class="guide">
-    <h1>排课任务</h1>
-    <div class="steps">
-      <el-steps style="max-width: 600px" :active="active" finish-status="success">
-    <el-step title="选择班级" />
-    <el-step title="数据选择" />
-    <el-step title="排课中.." />
-    <el-step title="排课结果" />
-  </el-steps>
+      <h1>排课任务</h1>
+      <div class="steps">
+        <el-steps style="max-width: 600px" :active="active" finish-status="success">
+          <el-step title="选择班级" />
+          <el-step title="数据选择" />
+          <el-step title="排课中.." />
+          <el-step title="排课结果" />
+        </el-steps>
+      </div>
     </div>
-  </div>
-  <div v-show="active === 0" class="chooseclass">
-      <chooseclass />
-  </div>
-  <el-button style="margin-top: 12px" @click="next">下一步</el-button>
+    <div v-show="active === 0" class="chooseclass-container">
+      <div class="chooseclass">
+        <chooseclass />
+        <el-button class="next-button" @click="next">下一步</el-button>
+      </div>
+    </div>
+    <div v-show="active > 0">
+      <el-button style="margin-top: 12px" @click="next">下一步</el-button>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup name="ScheduleManagement">
+<script setup lang="ts" name="ScheduleManagement">
 import { ref } from 'vue'
 import chooseclass from '@/components/ManualScheduling/chooseclass.vue'
 
@@ -30,25 +35,33 @@ const next = () => {
 </script>
 
 <style scoped>
-.index{
+.index {
   display: flex;
   flex-direction: column;
-  align-items: center;
 }
-.guide{
+.guide {
   display: flex;  
   width: 100%;
 }
-.steps{
-  margin-left:60px;
-  width:1200px;
+.steps {
+  margin-left: 60px;
+  width: 1200px;
   position: relative;
   top: 30px;
 }
-.chooseclass{
+.chooseclass-container {
   width: 100%;
-  position:relative;
-  top:150px;
-  left:500px;
+  position: relative;
+  top: 150px;
+}
+.chooseclass {
+  width: 100%;
+  padding-left: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.next-button {
+  margin-top: 20px;
 }
 </style>
